992aef6844e65e80eed3d20f3c07b95ccae1e481
Jyotiranjan Patra <112277029+Jrpatra706@users.noreply.github.com>
2023-11-18 11:39:38 +0530

    used localStorage for cart array

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,18 @@
-export let cart =[{
-    id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:2
-},{
-    id:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:1
-}];
+export let cart =JSON.parse(localStorage.getItem('cart'));
 
+if (!cart){
+    cart = [{
+        id:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2
+    },{
+        id:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1
+    }];
+}
+
+function updateCart(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 // add to cart array
 export function addToCart(button){
     let item;
@@ -23,6 +30,7 @@ export function addToCart(button){
             quantity:1
         });
     }
+    updateCart();
 }
  
 export function deleteFromCart(productId){
@@ -33,4 +41,5 @@ export function deleteFromCart(productId){
       }
     });
     cart=newCart;
+    updateCart();
 }
